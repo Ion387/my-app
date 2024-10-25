62296b4ceb8271a28beab02d2f9159fc5e2554f7
Ion387 <ion06@bk.ru>
2024-10-25 15:34:45 +0300

    ss

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-/* import { BrowserRouter, Route, Routes } from 'react-router-dom'; */
+ import { HashRouter, Route, Routes } from 'react-router-dom'; 
 import Main from './Components/Content/Main';
 
  function App() {
   return (
+    <HashRouter>
     <div>
 Деплооой!
      <header >
       </header>
       <nav>Деплой</nav> 
-{/*       <Routes>
+      <Routes>
       <Route path="/" element={<Main/>}></Route>
-      </Routes> */}
-      <footer></footer>
+      </Routes> 
+      <footer>23423423</footer>
       </div>  
+      </HashRouter>
   ); 
 }
 export default App;
