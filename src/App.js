fa3fb48618b058a0e5dc424442e07d5807768b6f
Ion387 <ion06@bk.ru>
2024-10-25 15:20:03 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+/* import { BrowserRouter, Route, Routes } from 'react-router-dom'; */
 import Main from './Components/Content/Main';
 
  function App() {
   return (
-    <BrowserRouter>
-
+    <div>
+Деплооой!
      <header >
       </header>
       <nav>Деплой</nav> 
-      <Routes>
+{/*       <Routes>
       <Route path="/" element={<Main/>}></Route>
-      </Routes>
+      </Routes> */}
       <footer></footer>
-      </BrowserRouter>  
+      </div>  
   ); 
 }
 export default App;
